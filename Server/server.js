ff31e5866f8a576ee1617e90d9e24743c736ea73
Ishika2905 <126647254+Ishika2905@users.noreply.github.com>
2024-11-05 09:26:07 +0530

    5 Nov 2024

---
+++ Server/server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const connectDb = require("./config/dbConnection");
+const errorHandler = require("./middlewares/errorHandler");
+const cors= require("cors");
+const hbs = require("hbs");
+const path = require("path");
+
+const users = [
+    { name: "Harman Dhiman", age: 20 },
+    { name: "Hindveer", age: 19 },
+    { name: "Jaikirat", age: 20 },
+];
+const app = express();
+const port = process.env.PORT || 5000;
+const dotenv = require("dotenv");
+ dotenv.config();
+ connectDb();
+app.use(express.json());
+app.use(cors());
+app.get('/' , (req , res)=>{
+    res.send("working");
+})
+app.set('view engine' , 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
+app.get("/home",(req , res)=>{
+    res.render("home" , {
+       username:" Harman Dhiman",
+       posts : " time pass"
+    })
+})
+app.get("/alluser", (req, res) => {
+    res.render("alluser", {
+        users: users, 
+    });
+});
+//register route
+app.use("/api/register" , require("./Routes/userroutes"));
+app.listen(port , ()=>{
+    console.log(`server running on http://localhost:${port}`);
+})
